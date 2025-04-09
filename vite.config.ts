a0e412fc59f1b8e35bce9c8d1d6d4158ec39244d
Charles Mbillo <82718900+CharlesMbillo@users.noreply.github.com>
2025-04-09 05:06:40 +0000

    preview

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -10,16 +9,15 @@ export default defineConfig(({ mode }: { mode: string }) => ({
     host: "::",
     port: 8080,
     allowedHosts: [
-      "n65rv6-8080.csb.app", 
-      "localhost", 
-      "a0d15119-2d55-4bd9-8204-fa934b3891b2.lovableproject.com"
-    ]
+      "72qngm-8080.csb.app",
+      "n65rv6-8080.csb.app",
+      "localhost",
+      "a0d15119-2d55-4bd9-8204-fa934b3891b2.lovableproject.com",
+    ],
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
